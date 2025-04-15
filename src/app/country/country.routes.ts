0c0a7f8895f2ba225d380ff39abf221094365995
JaviSrz <ljsuarez7@gmail.com>
2025-04-15 11:00:43 +0200

    Creadas paginas base y rutas base además del layout para la parte de paises.

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { CountryLayoutComponent } from './layouts/country-layout/country-layout.component';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+
+export const countryRoutes: Routes = [
+  {
+    path: '',
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      }
+    ]
+  },
+];
+
+export default countryRoutes;
