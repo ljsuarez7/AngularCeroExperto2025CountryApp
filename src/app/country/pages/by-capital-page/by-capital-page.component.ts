0c1dd4bfecf9dc8ffd55e5d9accd6c3f9f77be95
JaviSrz <ljsuarez7@gmail.com>
2025-04-18 10:36:03 +0200

    implementación de la busqueda por capital usando resource de angular 19+

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryService } from '../../services/country.service';
 import type { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -12,30 +13,16 @@ import type { Country } from '../../interfaces/country.interface';
 export class ByCapitalPageComponent {
 
   countryService = inject(CountryService);
+  query = signal<string>('');
 
-  isLoading = signal<boolean>(false);
-  isError = signal<string|null>(null);
-  countries = signal<Country[]>([]);
-
-  onSearch(query: string){
-
-    if(this.isLoading()) return;
-
-    this.isLoading.set(true);
-    this.isError.set(null);
-
-    this.countryService.searchByCapital(query).subscribe({
-      next: (countries) => {
-        this.isLoading.set(false);
-        this.countries.set(countries);
-      },
-      error: (err) => {
-        this.isLoading.set(false);
-        this.countries.set([]);
-        this.isError.set(err);
-      }
-    });
-
-  }
+  countryResource = resource({
+    request: () => ({query: this.query()}),
+    loader: async({request}) => {
+      if(!request.query) return [];
+      return await firstValueFrom(
+        this.countryService.searchByCapital(request.query)
+      );
+    }
+  });
 
 }
